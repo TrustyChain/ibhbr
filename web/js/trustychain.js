7c004fc8fc88017a43197ee1008bc37a2669385a
Esdras Eduardo <esdrasedu@gmail.com>
2016-09-08 01:57:17 -0300

    Fix campaign

--- web/js/trustychain.js
+++ web/js/trustychain.js
@@ -39,43 +39,31 @@ var campaignAddress = '0x6B312C8bF724C153777d15565181763ad328B72c';
 
 var abiCommunityContract = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"communitiesToOwner","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"communityId","type":"uint256"},{"name":"_member","type":"address"},{"name":"_name","type":"bytes32"}],"name":"addMember","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"bytes32"},{"name":"_description","type":"bytes32"}],"name":"createComunity","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"members","outputs":[{"name":"uuid","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"}],"type":"function"},{"constant":false,"inputs":[{"name":"communityId","type":"uint256"},{"name":"_member","type":"address"}],"name":"getMemberName","outputs":[{"name":"","type":"bytes32"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"name":"communitiesToMembers","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"communities","outputs":[{"name":"uuid","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"},{"name":"description","type":"bytes32"}],"type":"function"}];
 
+
+var abiCampaignContract = [{"constant":false,"inputs":[{"name":"_campaign","type":"uint256"}],"name":"concludeCampaign","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"campaigns","outputs":[{"name":"uuid","type":"uint256"},{"name":"communityId","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"},{"name":"description","type":"bytes32"},{"name":"image","type":"bytes32"},{"name":"amountNeed","type":"uint256"},{"name":"amountReceive","type":"uint256"},{"name":"done","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"collaborators","outputs":[{"name":"uuid","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"},{"name":"description","type":"bytes32"},{"name":"amount","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_campaign","type":"uint256"},{"name":"amount","type":"uint256"}],"name":"donateToCampaign","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"name":"campaignToCollaborates","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_campaign","type":"uint256"},{"name":"_collaborator","type":"address"},{"name":"_description","type":"bytes32"},{"name":"_amount","type":"uint256"}],"name":"addCollaborate","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_community","type":"uint256"},{"name":"_name","type":"bytes32"},{"name":"_description","type":"bytes32"},{"name":"_image","type":"bytes32"}],"name":"createCampaign","outputs":[],"type":"function"},{"constant":true,"inputs":[],"name":"community","outputs":[{"name":"","type":"address"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"ownerToCampaigns","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"inputs":[{"name":"_community","type":"address"}],"type":"constructor"}];
+
 /* Carregamento dos Contratos */
 var communityContract = web3.eth.contract(abiCommunityContract);
 var community = communityContract.at(communityAddress);
 
-
-var keystore;
-
-
-// var password = 'mercadopokemon';
+var campaignContract = web3.eth.contract(abiCampaignContract);
+var campaign = communityContract.at(campaignAddress);
 
 /* Inicializa conexão com o node local */
-web3.setProvider(new web3.providers.HttpProvider());	
-
-/*function setWeb3Provider(keystore) {
-    var web3Provider = new HookedWeb3Provider({
-      host: "http://localhost:8545"
-      ,transaction_signer: keystore
-    });
-
-    web3.setProvider(web3Provider);
-};
-
-setWeb3Provider();
- */
+web3.setProvider(new web3.providers.HttpProvider());
 
 var accountAddress = web3.eth.accounts;
-
-alert(accountAddress[0]);
 
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 function createCommunity(){
-
-    community.createComunity(web3.fromAscii('_name'), web3.fromAscii('_description'), {value: 0, gas: 428638, gasPrice: 20000000000}, function(error, result){
+  community.createComunity(web3.fromAscii('_name'), web3.fromAscii('_description'), {value: 0, gas: 428638, gasPrice: 20000000000}, function(error, result){
         console.dir(arguments);
-    })
-
+  });
 }
 
-createCommunity();
+function createCampaign(name, description, image){
+    community.createComunity(campaignId, web3.fromAscii(name), web3.fromAscii(description), web3.fromAscii(image), {value: 0, gas: 428638, gasPrice: 20000000000}, function(error, result){
+        console.dir(arguments);
+  });
+}
