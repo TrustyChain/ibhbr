e98f9d0f096d4513e889dff144b0bb53c1caa2be
Esdras Eduardo <esdrasedu@gmail.com>
2016-09-08 01:43:53 -0300

    Clean web3

--- web/js/trustychain.js
+++ web/js/trustychain.js
@@ -23,127 +23,21 @@ Copyright (c) 2016 Edilson Osorio Junior - OriginalMy.com
 var Web3 = require('web3');
 var web3 = new Web3();
 var eth = web3.eth;
-	
+
 var txutils = lightwallet.txutils;
 var signing = lightwallet.signing;
 var encryption = lightwallet.encryption;
 
 
+//Renato
 var communityAddress = '0xa5a886bdba40e3906371ce8066faef90b2adedce';
-var abiCommunityContract = [
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "communitiesToOwner",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "_name",
-            "type": "bytes32"
-          },
-          {
-            "name": "_description",
-            "type": "bytes32"
-          }
-        ],
-        "name": "create",
-        "outputs": [],
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "communityId",
-            "type": "uint256"
-          },
-          {
-            "name": "member",
-            "type": "address"
-          }
-        ],
-        "name": "removeMember",
-        "outputs": [],
-        "type": "function"
-      },
-      {
-        "constant": false,
-        "inputs": [
-          {
-            "name": "communityId",
-            "type": "uint256"
-          },
-          {
-            "name": "member",
-            "type": "address"
-          }
-        ],
-        "name": "addMember",
-        "outputs": [],
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "",
-            "type": "address"
-          }
-        ],
-        "name": "communitiesToMembers",
-        "outputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "type": "function"
-      },
-      {
-        "constant": true,
-        "inputs": [
-          {
-            "name": "",
-            "type": "uint256"
-          }
-        ],
-        "name": "communities",
-        "outputs": [
-          {
-            "name": "uuid",
-            "type": "uint256"
-          },
-          {
-            "name": "owner",
-            "type": "address"
-          },
-          {
-            "name": "name",
-            "type": "bytes32"
-          },
-          {
-            "name": "description",
-            "type": "bytes32"
-          }
-        ],
-        "type": "function"
-      }
-    ];
+var campaignAddress = '';
 
+//Eduardo
+var communityAddress = '0xdC3B778F1459EDa14c5e6bA4BE55f25244AadE62';
+var campaignAddress = '0x6B312C8bF724C153777d15565181763ad328B72c';
+
+var abiCommunityContract = [{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"communitiesToOwner","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"communityId","type":"uint256"},{"name":"_member","type":"address"},{"name":"_name","type":"bytes32"}],"name":"addMember","outputs":[],"type":"function"},{"constant":false,"inputs":[{"name":"_name","type":"bytes32"},{"name":"_description","type":"bytes32"}],"name":"createComunity","outputs":[],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"members","outputs":[{"name":"uuid","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"}],"type":"function"},{"constant":false,"inputs":[{"name":"communityId","type":"uint256"},{"name":"_member","type":"address"}],"name":"getMemberName","outputs":[{"name":"","type":"bytes32"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"name":"communitiesToMembers","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"communities","outputs":[{"name":"uuid","type":"uint256"},{"name":"owner","type":"address"},{"name":"name","type":"bytes32"},{"name":"description","type":"bytes32"}],"type":"function"}];
 
 /* Carregamento dos Contratos */
 var communityContract = web3.eth.contract(abiCommunityContract);
@@ -156,9 +50,9 @@ var keystore;
 // var password = 'mercadopokemon';
 
 /* Inicializa conexão com o node local */
-//web3.setProvider(new web3.providers.HttpProvider());	
+web3.setProvider(new web3.providers.HttpProvider());	
 
-function setWeb3Provider(keystore) {
+/*function setWeb3Provider(keystore) {
     var web3Provider = new HookedWeb3Provider({
       host: "http://localhost:8545"
       ,transaction_signer: keystore
@@ -167,47 +61,21 @@ function setWeb3Provider(keystore) {
     web3.setProvider(web3Provider);
 };
 
-setWeb3Provider();  
-    
+setWeb3Provider();
+ */
+
 var accountAddress = web3.eth.accounts;
 
 alert(accountAddress[0]);
-alert(accountAddress[1]);
+
+web3.eth.defaultAccount = web3.eth.accounts[0];
 
 function createCommunity(){
-    
-    community.create(web3.fromAscii('_name'), web3.fromAscii('_description'))
+
+    community.createComunity(web3.fromAscii('_name'), web3.fromAscii('_description'), {value: 0, gas: 428638, gasPrice: 20000000000}, function(error, result){
+        console.dir(arguments);
+    })
 
 }
 
 createCommunity();
-
-/* Criação/carregamento do account */
-lightwallet.keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey) {
-
-	/* Se for criar uma nova conta com um novo seed, descomente as duas linhas abaixo */
-
-	//var extraEntropy = 'mercadopokemon129387612348917236';
-	//seed = lightwallet.keystore.generateRandomSeed(extraEntropy);
-	
-	keystore = new lightwallet.keystore(seed, pwDerivedKey);
-	keystore.generateNewAddress(pwDerivedKey, 1);
-
-	accountAddress = keystore.getAddresses()[0];
-
-	document.getElementById("accountAddress").innerText = '0x'+accountAddress;
-	document.getElementById("seed").innerText = seed;
-		
-	web3.eth.defaultAccount = eth.accounts[0];
-
-});	
-
-
-
-/* Filtro do web3 para atualizar os valores automaticamente */
-var filter = web3.eth.filter('latest');
-filter.watch(function(error, result) {
-	if (!error) {
-		showStatus()
-	}
-});
